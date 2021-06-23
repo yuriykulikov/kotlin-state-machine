3ff785c66a28ec5d63d8848e11705427695254fc
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2021-06-23 13:35:26 +0200

    build(gradle): added gradle

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.15.0")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    // adds aggregateJacocoReport
+    // see https://kordamp.org/kordamp-gradle-plugins/#_task_aggregate_jacoco_report
+    id("org.kordamp.gradle.jacoco") version "0.45.0"
+}
+
+subprojects {
+    group = "io.github.yuriykulikov"
+    version = "0.1.1-SNAPSHOT"
+}
